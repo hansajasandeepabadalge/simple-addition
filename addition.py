a9c17b600501cc427aeeb29774cd7e3b2e09aa2f
Hansaja Sandeepa <76199476+hansajasandeepabadalge@users.noreply.github.com>
2024-07-05 01:00:23 +0530

    first commit

---
+++ addition.py
@@ -0,0 +1,10 @@
+#User inputs 
+
+a = int(input("Enter your First Number: "))
+b = int(input("Enter Your Second Number: "))
+
+#Adding Number by using User inputs
+
+answer = a + b
+
+print("The Sum of the two numbers is:", answer)
